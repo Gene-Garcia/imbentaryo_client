4a59f4a782a5b671acfd8f9c48349316c707411c
gn-grc <genejogarcia.gg@gmail.com>
2022-05-11 15:21:52 +0800

    layout of add item and configuration on spinner population

--- imbentaryo_client/Fragments/FragmentAddItem.cs
+++ imbentaryo_client/Fragments/FragmentAddItem.cs
@@ -14,17 +14,62 @@ namespace imbentaryo_client.Fragments
 {
     public class FragmentAddItem : AndroidX.Fragment.App.Fragment
     {
+        // form components
+        EditText itemNameEditText;
+        Spinner itemGroupDropDown;
+        EditText stockCountEditText;
+        EditText remarksEditText;
+        Button addItemBtn;
+
+
+        // spinner value holder
+        List<string> groupNames;
+        List<string> groupIds;
         public override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             // Create your fragment here
+            groupNames = new List<string>();
+            groupIds = new List<string>();
+
+            // populate by making api request
+            groupNames.Add("Abc1");
+            groupIds.Add("Abc1id");
+
+            groupNames.Add("Abc2");
+            groupIds.Add("Abc2id");
+
+            groupNames.Add("Abc3");
+            groupIds.Add("Abc3id");
+
+            groupNames.Add("Abc4");
+            groupIds.Add("Abc4id");
+
+            groupNames.Add("Abc5");
+            groupIds.Add("Abc5id");
         }
 
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
+            //base.OnCreateView(inflater, container, savedInstanceState);
+
             // Use this to return your custom view for this Fragment
-            return inflater.Inflate(Resource.Layout.fragment_add_item, container, false);
+            View view = inflater.Inflate(Resource.Layout.fragment_add_item, container, false);
+
+            // itemNameEditTxt itemGroupDropDown stockCountEditText remarksEditText addItemBtn
+            this.itemNameEditText = view.FindViewById<EditText>(Resource.Id.itemNameEditText);
+            this.itemGroupDropDown = view.FindViewById<Spinner>(Resource.Id.itemGroupDropDown);
+            this.stockCountEditText = view.FindViewById<EditText>(Resource.Id.stockCountEditText);
+            this.remarksEditText = view.FindViewById<EditText>(Resource.Id.remarksEditText);
+            this.addItemBtn = view.FindViewById<Button>(Resource.Id.addItemBtn);
+
+            // populate spinner
+            ArrayAdapter spinnerAdapter = new ArrayAdapter<string>(this.Activity, Resource.Layout.support_simple_spinner_dropdown_item, this.groupNames);
+            itemGroupDropDown.Adapter = spinnerAdapter;
+
+
+            return view;
         }
     }
 }
