daaaa36dc5d8c0569b7ad193b745f5bb21657a89
gn-grc <genejogarcia.gg@gmail.com>
2022-05-11 12:05:38 +0800

    initial fragments

---
+++ imbentaryo_client/Fragments/FragmentItemsView.cs
@@ -0,0 +1,29 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace imbentaryo_client.Fragments
+{
+    public class FragmentItemsView : AndroidX.Fragment.App.Fragment
+    {
+        public override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your fragment here
+        }
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            return inflater.Inflate(Resource.Layout.fragment_items_view, container, false);
+        }
+    }
+}
