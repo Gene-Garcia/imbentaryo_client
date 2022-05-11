c70a4ded3077d05acde41d8039c965fc1bcca8eb
gn-grc <genejogarcia.gg@gmail.com>
2022-05-11 21:30:58 +0800

    added http clients

---
+++ imbentaryo_client/Http/ItemGroupService.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using imbentaryo_client.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace imbentaryo_client.Http
+{
+    internal class ItemGroupService : Service
+    {
+        private const string HIGH_PATH = "/group";
+        private string uri;
+
+        public ItemGroupService() : base()
+        {
+            this.uri = END_POINT + HIGH_PATH;
+        }
+
+        public async Task<List<ItemGroup>> GetItemGroups()
+        {
+            List<ItemGroup> itemGroups = new List<ItemGroup>();
+
+            Uri uri = new Uri(this.uri + "/all");
+
+            HttpResponseMessage response = await client.GetAsync(uri);
+            if (response.IsSuccessStatusCode)
+            {
+                string content = await response.Content.ReadAsStringAsync();
+                itemGroups = JsonConvert.DeserializeObject<List<ItemGroup>>(content);
+            }
+
+            return itemGroups;
+
+        }
+    }
+}
---
+++ imbentaryo_client/Http/Service.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+
+namespace imbentaryo_client.Http
+{
+    internal class Service
+    {
+        protected const string END_POINT = "http://192.168.1.9:3000/v1";
+
+        protected HttpClient client; 
+
+        public Service()
+        {
+            this.client = new HttpClient();
+        }
+    }
+}
