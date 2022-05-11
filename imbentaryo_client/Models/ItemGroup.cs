d3df15cdbf58696d39fe411c60f14d3cc8ea4d9b
gn-grc <genejogarcia.gg@gmail.com>
2022-05-11 21:30:45 +0800

    added item grup model with json data annotation label

---
+++ imbentaryo_client/Models/ItemGroup.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace imbentaryo_client.Models
+{
+    public class ItemGroup
+    {
+        [JsonProperty("group_id")]
+        public string Id { get; set; }
+
+        [JsonProperty("group_name")]
+        public string Name { get; set; }
+
+        [JsonProperty("remarks")]
+        public string Remarks { get; set; }
+    }
+}
