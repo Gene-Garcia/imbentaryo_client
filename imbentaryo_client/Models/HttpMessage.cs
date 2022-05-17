b2581f95ef804285ed1aa3e74a5760594bfe30cf
gn-grc <genejogarcia.gg@gmail.com>
2022-05-17 23:22:20 +0800

    added model for httpmessage to be returned to activities/fragments

---
+++ imbentaryo_client/Models/HttpMessage.cs
@@ -0,0 +1,21 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace imbentaryo_client.Models
+{
+    internal class HttpMessage
+    {
+        [JsonProperty("message")]
+        public string Message { get; set; }
+        public string StatusCode { get; set; }
+    }
+}
