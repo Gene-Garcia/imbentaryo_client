ef50068d14ca57b3e23f3997937565c22131ee1f
gn-grc <genejogarcia.gg@gmail.com>
2022-05-18 23:49:18 +0800

    implemented dynamic list for item inventory

---
+++ imbentaryo_client/Adapters/ItemInventoryAdapter.cs
@@ -0,0 +1,74 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using imbentaryo_client.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace imbentaryo_client.Adapters
+{
+    internal class ItemInventoryAdapter : BaseAdapter
+    {
+
+        Context context;
+
+        private Activity activity;
+        private List<Item> items;
+
+        public ItemInventoryAdapter(Activity activity, List<Item> items)
+        {
+            this.activity = activity;
+            this.items = items;
+        }
+
+
+        public override Java.Lang.Object GetItem(int position)
+        {
+            return null;
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var view = convertView ?? this.activity.LayoutInflater.Inflate(Resource.Layout._item_inventory_template, parent, false);
+
+            // initiate components
+            TextView itemName = view.FindViewById<TextView>(Resource.Id.itemName);
+            TextView stockCount = view.FindViewById<TextView>(Resource.Id.stockCount);
+            TextView inventoryDate = view.FindViewById<TextView>(Resource.Id.inventoryDate);
+
+            //fill in your items
+            itemName.Text = this.items[position].Name;
+            stockCount.Text = this.items[position].Stock.ToString();
+            inventoryDate.Text = this.items[position].DateAdded;
+
+            return view;
+        }
+
+        //Fill in cound here, currently 0
+        public override int Count
+        {
+            get
+            {
+                return items.Count;
+            }
+        }
+
+    }
+
+    internal class ItemInventoryAdapterViewHolder : Java.Lang.Object
+    {
+        public TextView itemName { get; set; }
+        public TextView stockCount { get; set; }
+        public TextView inventoryDate { get; set; }
+    }
+}
--- imbentaryo_client/Fragments/FragmentItemsView.cs
+++ imbentaryo_client/Fragments/FragmentItemsView.cs
@@ -5,6 +5,8 @@ using Android.Runtime;
 using Android.Util;
 using Android.Views;
 using Android.Widget;
+using imbentaryo_client.Adapters;
+using imbentaryo_client.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,9 +23,201 @@ namespace imbentaryo_client.Fragments
             // Create your fragment here
         }
 
+        ListView inventoryItemsListView;
+
         public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
         {
-            return inflater.Inflate(Resource.Layout.fragment_items_view, container, false);
+            View view = inflater.Inflate(Resource.Layout.fragment_items_view, container, false);
+
+            this.inventoryItemsListView = view.FindViewById<ListView>(Resource.Id.inventoryItemsListView);
+
+            return view;
+        }
+
+
+        public override void OnResume()
+        {
+            base.OnResume();
+
+            // set up adapter
+            ItemInventoryAdapter adapter = new ItemInventoryAdapter(this.Activity, GetItems());
+            this.inventoryItemsListView.Adapter = adapter;
+        }
+
+        private List<Item> GetItems()
+        {
+
+            List<Item> items = new List<Item>();
+
+            items.Add(new Item()
+            {
+                Name = "Test",
+                DateAdded = "5/18/2022",
+                Stock = 15
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test 2",
+                DateAdded = "4/5/2022",
+                Stock = 54
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test 3",
+                DateAdded = "12/11/2022",
+                Stock = 50
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test 4",
+                DateAdded = "1/7/2022",
+                Stock = 15
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test",
+                DateAdded = "5/18/2022",
+                Stock = 15
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test 2",
+                DateAdded = "4/5/2022",
+                Stock = 54
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test 3",
+                DateAdded = "12/11/2022",
+                Stock = 50
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test 4",
+                DateAdded = "1/7/2022",
+                Stock = 15
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test",
+                DateAdded = "5/18/2022",
+                Stock = 15
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test 2",
+                DateAdded = "4/5/2022",
+                Stock = 54
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test 3",
+                DateAdded = "12/11/2022",
+                Stock = 50
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test 4",
+                DateAdded = "1/7/2022",
+                Stock = 15
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test",
+                DateAdded = "5/18/2022",
+                Stock = 15
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test 2",
+                DateAdded = "4/5/2022",
+                Stock = 54
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test 3",
+                DateAdded = "12/11/2022",
+                Stock = 50
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test 4",
+                DateAdded = "1/7/2022",
+                Stock = 15
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test",
+                DateAdded = "5/18/2022",
+                Stock = 15
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test 2",
+                DateAdded = "4/5/2022",
+                Stock = 54
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test 3",
+                DateAdded = "12/11/2022",
+                Stock = 50
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test 4",
+                DateAdded = "1/7/2022",
+                Stock = 15
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test",
+                DateAdded = "5/18/2022",
+                Stock = 15
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test 2",
+                DateAdded = "4/5/2022",
+                Stock = 54
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test 3",
+                DateAdded = "12/11/2022",
+                Stock = 50
+            });
+
+            items.Add(new Item()
+            {
+                Name = "Test 4",
+                DateAdded = "1/7/2022",
+                Stock = 15
+            });
+
+            return items;
         }
     }
 }
