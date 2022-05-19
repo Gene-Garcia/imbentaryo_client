1887b27aa6b78a78e06a8d9eef446ab9ccb0cd7c
gn-grc <genejogarcia.gg@gmail.com>
2022-05-20 00:32:58 +0800

    added model utility to convert an api-sent format model to formal modal

---
+++ imbentaryo_client/Models/Utilities/ModelConverter.cs
@@ -0,0 +1,43 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using imbentaryo_client.Models;
+using imbentaryo_client.Http;
+
+namespace imbentaryo_client.Models.Utilities
+{
+    internal class ModelConverter
+    {
+        public static List<Inventory> ConvertToItem(List<ItemAPIModel> itemAPIModel)
+        {
+            List<Inventory> items = new List<Inventory>();
+
+            foreach (ItemAPIModel itemApi in itemAPIModel)
+            {
+                Item tempItem = new Item()
+                {
+                    Id = itemApi.ItemId,
+                    Name = itemApi.Name
+                };
+
+                Inventory tempInventory = new Inventory()
+                {
+                    Item = tempItem,
+                    Updated = itemApi.Updated,
+                    Quantity = int.Parse(itemApi.Quantity)
+                };
+
+                items.Add(tempInventory);
+            }
+
+            return items;
+        }
+    }
+}
