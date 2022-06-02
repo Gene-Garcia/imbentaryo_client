203498027eadeb51ee056fa52f45c826c05cc481
gn-grc <genejogarcia.gg@gmail.com>
2022-06-03 00:17:55 +0800

    added a session helper to access shared preference and its contents

---
+++ imbentaryo_client/Utils/UserSession.cs
@@ -0,0 +1,33 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace imbentaryo_client.Utils
+{
+    internal class UserSession
+    {
+        public ISharedPreferences session;
+        
+        public UserSession()
+        {
+            session = Application.Context.GetSharedPreferences("UserSession", FileCreationMode.Private);
+        }
+
+        public string GetAccountId()
+        {
+            return session.GetString("accountId", String.Empty);
+        }
+
+        public string GetAccountUsername()
+        {
+            return session.GetString("username", String.Empty);
+        }
+    }
+}
